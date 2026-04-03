7ae6d287736ffefc10f494337fe46d8a8d7eb45a
simo-bimo <simon@anldowd.com>
2026-04-04 09:20:52 +1100

    Fix: Adjusted app id in archive script.

--- code/MakePackage.sh
+++ code/MakePackage.sh
@@ -5,17 +5,17 @@
 
 REPOURL="https://github.com/simo-bimo/nextcloud-hide-user-emails.git" #official repo, change if forked
 SIGN=FALSE #Sign the file for uploading to the official appstore
-SIGNCERT=~/.nextcloud/certificates/hide-user-emails.key #The location of the key
+SIGNCERT=~/.nextcloud/certificates/hideuseremails.key #The location of the key
 
 mkdir tmp
 
 echo "Cloning $REPOURL"
-git clone -q $REPOURL tmp/hide-user-emails/
-VERSION=$(xmlstarlet sel -T -t -m '//info/version' -v . -n tmp/hide-user-emails/appinfo/info.xml)
+git clone -q $REPOURL tmp/hideuseremails/
+VERSION=$(xmlstarlet sel -T -t -m '//info/version' -v . -n tmp/hideuseremails/appinfo/info.xml)
 echo "Version is ${VERSION}"
 
 echo "Starting compression"
-tar --exclude='code' --exclude='differences.png' --exclude=".git" -czf hide-user-emails-v${VERSION}.tar.gz -C tmp hide-user-emails/
+tar --exclude='code' --exclude='differences.png' --exclude=".git" -czf hideuseremails-v${VERSION}.tar.gz -C tmp hideuseremails/
 
 rm -rf tmp
 
@@ -23,6 +23,6 @@ if [ $SIGN ]; then
 	echo ""
 	echo "The signature:"
 	echo ""
-	openssl dgst -sha512 -sign $SIGNCERT hide-user-emails-v${VERSION}.tar.gz | openssl base64
+	openssl dgst -sha512 -sign $SIGNCERT hideuseremails-v${VERSION}.tar.gz | openssl base64
 	echo ""
 fi
